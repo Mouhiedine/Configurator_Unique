3034dd0e4ad7e375cc88267f2a9847e2f342b76d
mouhiedine <b.mouhiedine@hotmail.fr>
2022-09-04 11:14:05 +0200

    main

---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,219 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "cold%20white.png",
+                    "multimaterials": [
+                        "116+112"
+                    ],
+                    "name": "Cold White"
+                },
+                {
+                    "icon": "deep%20black.png",
+                    "multimaterials": [
+                        "116+113"
+                    ],
+                    "name": "Deep Black"
+                },
+                {
+                    "checked": true,
+                    "icon": "carmine.png",
+                    "multimaterials": [
+                        "116+114"
+                    ],
+                    "name": "Carmine"
+                },
+                {
+                    "icon": "calamine%20blue.png",
+                    "multimaterials": [
+                        "116+115"
+                    ],
+                    "name": "Calamine Blue"
+                }
+            ],
+            "name": "Upper Parts"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "calamine%20blue-2.png",
+                    "multimaterials": [
+                        "111+107"
+                    ],
+                    "name": "Calamine Blue"
+                },
+                {
+                    "checked": true,
+                    "icon": "carmine-2.png",
+                    "multimaterials": [
+                        "111+108"
+                    ],
+                    "name": "Carmine"
+                },
+                {
+                    "icon": "cold%20white-2.png",
+                    "multimaterials": [
+                        "111+109"
+                    ],
+                    "name": "Cold White"
+                },
+                {
+                    "icon": "deep%20black-2.png",
+                    "multimaterials": [
+                        "111+110"
+                    ],
+                    "name": "Deep Black"
+                }
+            ],
+            "name": "Frame"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "rough%20black.png",
+                    "multimaterials": [
+                        "106+102"
+                    ],
+                    "name": "Rough Black"
+                },
+                {
+                    "icon": "rough%20blue.png",
+                    "multimaterials": [
+                        "106+103"
+                    ],
+                    "name": "Rough Blue"
+                },
+                {
+                    "checked": true,
+                    "icon": "rough%20red.png",
+                    "multimaterials": [
+                        "106+104"
+                    ],
+                    "name": "Rough Red"
+                },
+                {
+                    "icon": "rough%20white.png",
+                    "multimaterials": [
+                        "106+105"
+                    ],
+                    "name": "Rough white"
+                }
+            ],
+            "name": "Exterior Parts"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "black%20metal.png",
+                    "multimaterials": [
+                        "122+118"
+                    ],
+                    "name": "Black Metal"
+                },
+                {
+                    "icon": "red%20metal.png",
+                    "multimaterials": [
+                        "122+119"
+                    ],
+                    "name": "Red Metal"
+                },
+                {
+                    "icon": "grey%20metal.png",
+                    "multimaterials": [
+                        "122+120"
+                    ],
+                    "name": "Grey Metal"
+                },
+                {
+                    "icon": "gold%20metal.png",
+                    "multimaterials": [
+                        "122+121"
+                    ],
+                    "name": "Gold Metal"
+                }
+            ],
+            "name": "Button"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "red.png",
+                    "multimaterials": [
+                        "128+125"
+                    ],
+                    "name": "Red"
+                },
+                {
+                    "icon": "blue.png",
+                    "multimaterials": [
+                        "128+126"
+                    ],
+                    "name": "Blue"
+                },
+                {
+                    "checked": true,
+                    "icon": "white.png",
+                    "multimaterials": [
+                        "128+127"
+                    ],
+                    "name": "White"
+                }
+            ],
+            "name": "Lights"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "gold.png",
+                    "multimaterials": [
+                        "139+135"
+                    ],
+                    "name": "Gold"
+                },
+                {
+                    "icon": "silver.png",
+                    "multimaterials": [
+                        "139+136"
+                    ],
+                    "name": "Silver"
+                },
+                {
+                    "icon": "pink%20gold%20plated.png",
+                    "multimaterials": [
+                        "139+137"
+                    ],
+                    "name": "Pink Gold Plated"
+                },
+                {
+                    "icon": "damascus%20steel.png",
+                    "multimaterials": [
+                        "139+138"
+                    ],
+                    "name": "Damascus Steel"
+                }
+            ],
+            "name": "Lions"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Upper Parts",
+                "Frame",
+                "Exterior Parts",
+                "Button",
+                "Lights",
+                "Lions"
+            ],
+            "modelset": 1004,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
